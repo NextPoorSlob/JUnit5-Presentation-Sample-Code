0f8200e246e0a6c15b5df607c6750b7bd7a6613a
Steve Gelman <nextpoorslob@gmail.com>
2019-07-15 20:11:35 -0500

    Error handling complete.

---
+++ junit5-demo-maven/b060-exception-handling-maven/src/main/java/com/nextpoorslob/junit5/b060/service/IntegerCalculator.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.nextpoorslob.junit5.b060.service;
+
+/**
+ * Implements a Calculator for Integers.  Note that a null is considered a zero.
+ * @author stephen
+ *
+ */
+public class IntegerCalculator implements Calculator<Integer> {
+
+	public Integer add(Integer addendLeft, Integer addendRight) {
+		
+		return (addendLeft != null ? addendLeft : 0)
+				+ (addendRight != null ? addendRight : 0);
+	}
+
+	public Integer subtract(Integer minuend, Integer subtrahend) {
+		
+		return (minuend != null ? minuend : 0)
+				- (subtrahend != null ? subtrahend : 0);
+	}
+
+	public Integer divide(Integer dividend, Integer divisor) {
+		return (dividend != null ? dividend : 0)
+				/ (divisor != null ? divisor : 0);
+	}
+}
